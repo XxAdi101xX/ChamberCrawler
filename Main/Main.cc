1f308384902c9ea91dacb4d11acb65c0721c1640
yf6wang <yf6wang@student.cs.uwaterloo.ca>
2016-11-29 02:11:43 -0500

    Implemented most of Main.cc, need to add some helpers, and add new entries to Defines.cc and Localisation.cc

--- Main/Main.cc
+++ Main/Main.cc
@@ -13,9 +13,10 @@ int main(int argc, char *argv[]) {
 	// game global variables
 	Generator rng = Generator{};
 	TextDisplay theTextDisplay = TextDisplay{};
-	int floorCount = 1;
+	int floorCount = 0;
 	Floor currentFloor = Floor{&theTextDisplay};
-	
+
+	bool NPCMovementPaused = false;	
 	bool merchantsAngered = false;
 	vector<PotionType> usedPotions;
 
@@ -28,11 +29,14 @@ int main(int argc, char *argv[]) {
 	// counters and temporaries
 	vector<shared_ptr<Character>> alreadyActed;
 	Cell* tempCell;
-	vector<Cell*> tempNeighbours;
+	vector<Cell*> tempNeighbourhood;
 	shared_ptr<Character> tempCharacter;
+	shared_ptr<Character> tempDefender;
 
 	vector<int> tempCoords;
 	vector<int> dragonCoords;
+
+	bool playerHasActed;
 
 	// number of attempts made to find valid neighbour
 	int neighbourAttemptsCounter = 0;
@@ -46,8 +50,6 @@ int main(int argc, char *argv[]) {
 	int numberOfSpawnedNPCs = 0;
 	Race tempRace;
 	int tempWallet;
-
-	
 
 	string cmd;
 
@@ -114,30 +116,30 @@ titleScreen:
 	player.attach(&theTextDisplay);
 
 
-newLevelStart:
+newFloorStart:
+	// updates floor count since this is a new floor
+	++floorCount;
+
 	// checks for win
-	if (floorCount == NUMBER_OF_FLOORS) {
+	if (floorCount > NUMBER_OF_FLOORS) {
 		cout << GAME_CLEARED_MSG << endl;
 		cout << FINAL_SCORE_MSG << ": " << player.getScore() << endl;
 		cout <<	"(" << CMD_YES << "|" << CMD_NO << ")    " 
 			<< REPLAY_PROMPT << endl;
 
-		while (true) {
-			if (!(cin >> cmd) || cmd == CMD_QUIT || CMD_NO) {
-				cout << GOODBYE_MSG << endl;
-				return 0;
-			}
+		if (!(cin >> cmd) || cmd == CMD_QUIT || CMD_NO) {
+			cout << GOODBYE_MSG << endl;
+			return 0;
+		}
 
-			else if (cmd == CMD_YES || cmd == CMD_RESTART) {
-				// clear game data
-				// NEED TO COMPLETE...........................................	
-				goto titleScreen;
-			}
+		else if (cmd == CMD_YES || cmd == CMD_RESTART) {
+			// clear game data
+			// NEED TO COMPLETE...........................................	
+			goto titleScreen;
+		}
 
-			else {
-				cout << INVALID_CMD_MSG << endl;
-			}
-
+		else {
+			cout << INVALID_CMD_MSG << endl;
 		}
 
 	}
@@ -203,16 +205,27 @@ newLevelStart:
 
 		// if the gold spawned was a dragon hoard
 		if (tempGoldValue == GOLD_PILE_DRAGON_HOARD_VALUE) {
-			do {
-				dragonCoords = genNeighbourCoord(tempCoord);
-				
-				// reroll if coordinates are not for a 
-				// FloorTile or has an occupant already
-			} while(!(isValidCoordinate(dragonCoords)) 
-				|| isOccupied(dragonCoords)));
+			try {
+				dragonCoords 
+					= getValidNeighbourCoordinates
+					(tempCoords);
 
-			(currentFloor.getCell(dragonCoords))
-				->setOccupant(createCharacter(Race::Dragon));
+	                        (currentFloor.getCell(dragonCoords))
+                                	->setOccupant
+					(createCharacter(Race::Dragon));
+
+			}
+
+			catch {
+				// remove the pile added
+				(currentFloor.getCell(tempCoords))
+					->setItem(nullptr);
+
+				// decrement counter to allow second try
+				--numberOfSpawnedGoldPiles;
+
+			}
+
 		}
 
 		++numberOfSpawnedGoldPiles;
@@ -239,85 +252,174 @@ newLevelStart:
 
 	// game start
 
-	// player turn start
 	while (player.getHP() != 0) {
+		// plater turn start
 		player.startTurnRoutine();
 
-		if (!(cin >> cmd) || cmd == CMD_QUIT) {
-			cout << GOODBYE_MSG << endl;
-			return 0;
-		}
+		while (!(playerHasActed)) {
+			playerHasActed = true; // default value
 
-		switch(cmd) {
-			case CMD_MOVE_NORTH :
-				break;
+			if (!(cin >> cmd) || cmd == CMD_QUIT) {
+				cout << GOODBYE_MSG << endl;
+				return 0;
+			}
 
-			case CMD_MOVE_SOUTH :
-				break;
+			switch(cmd) {
+				case CMD_MOVE_NORTH :
+					break;
 
-			case CMD_MOVE_EAST :
-				break;
+				case CMD_MOVE_SOUTH :
+					break;
 
-			case CMD_MOVE_WEST :
-				break;
+				case CMD_MOVE_EAST :
+					break;
 
-			case CMD_MOVE_NORTH_EAST :
-				break;
+				case CMD_MOVE_WEST :
+					break;
 
-                      	case CMD_MOVE_NORTH_WEST :
-                                break;
+				case CMD_MOVE_NORTH_EAST :
+					break;
 
-                        case CMD_MOVE_SOUTH_EAST :
-                                break;
+                      		case CMD_MOVE_NORTH_WEST :
+                                	break;
 
-                        case CMD_MOVE_SOUTH_WEST :
-                                break;
+                        	case CMD_MOVE_SOUTH_EAST :
+                                	break;
 
-                        case CMD_USE :
-                                break;
+                        	case CMD_MOVE_SOUTH_WEST :
+                                	break;
 
-                        case CMD_ATTACK :
-                                break;
+                        	case CMD_USE :
+                                	break;
 
-                        case CMD_STOP :
-                                break;
+                        	case CMD_ATTACK :
+                                	break;
 
-                        case CMD_RESTART :
-				// clears data
-				// TO BE IMPLEMENTED ......................................................
-                                break;
+                        	case CMD_STOP :
+                                	break;
 
-			default :
-			// do nothing if invalid command was entered
+                        	case CMD_RESTART :
+					// clears data
+					// TO BE IMPLEMENTED ......................................................
+                                	break;
+
+				default :
+					// invalid command
+
+					// makes sure to wait for player cmd
+					playerHasActed = false;
 		}
 
 		player.endTurnRoutine();
 
-		player.notifyObservers();
-	}
 
-	// NPC turn start
-	for (int i = 0; i < (currentFloor->getFloorDimension())[0]; ++i) {
-		for (int j = 0; j < (currentFloor->getFloorDimension())[1]; 
-			++j) {
-			// gets the all necessary info for this cell
-			tempCell = currentFloor.getCell(vector{i,j});
-			tempCharacter = tempCell->getOccupant;
-			tempNeighbours = tempCell->getNeighbours();
+		// NPC turn start
+		for (int i = 0; 
+			i < (currentFloor->getFloorDimension())[0]; ++i) {
+			for (int j = 0; 
+				j < (currentFloor->getFloorDimension())[1]; 
+				++j) {
 
-			if  (tempCharacter 
-				&& !(tempCharacter->getPlayerState())) {
+				// gets the all necessary info for this cell
+				tempCell = currentFloor.getCell(vector{i,j});
+				tempCharacter = tempCell->getOccupant();
+				tempNeighbourhood = tempCell->getNeighbours();
 
-				tempCharacter->startTurnRoutine();
+				if (tempCharacter 
+					&& !(tempCharacter
+					->getPlayerState())) {
 
-				tempCharacter->get
+					tempCharacter->startTurnRoutine();
+
+					for (auto neighbour: 
+						tempNeighbourhood) {
+						// checks if already acked
+						if (hasActed(tempCharacter)) {
+							break;
+						}
+
+						tempDefender 
+							= neighbour
+							->getOccupant();
+
+						// if player is beside NPC,
+						// and NPC is hostile
+						if (tempDefender
+							->getPlayerState()
+							&& tempCharacter
+							->getHostileState()) {
+							tempCharacter
+								->attack
+								(*tempDefender, 
+								rng);
+
+							// logs the attacker
+							alreadyActed
+								.emplace_back
+								(tempCharacter);
+							break;
+						}
+
+					}
+
+					if (!(hasActed(tempCharacter) 
+						&& !NPCMovementPaused)) {
+						try {
+							tempCharacter
+								->move
+								(getValidDirection);
+						}
+
+						catch{}
+
+						// logs the character 
+						// than just moved
+						alreadyActed
+						.emplace_back
+						(tempCharacter);
+					}
+
+					tempCharacter->endTurnRoutine();
+
+					// report updates to theTextDisplay
+					tempCharacter->notifyObservers();
+                               	}
+
+				// report updates to theTextDisplay
+				tempCell->notifyObservers();
+
 			}
 
 		}
 
+		// report updates to theTextDisplay
+		player.notifyObserver();
+
+		// outputs the new game state
+		cout << theTextDisplay << endl;
 	}
 
-	
+
+	// game over
+	cout << GAME_OVER_MSG << end;
+        cout << FINAL_SCORE_MSG << ": " << player.getScore() << endl;
+        cout << "(" << CMD_YES << "|" << CMD_NO << ")    "
+                << REPLAY_PROMPT << endl;
+
+        if (!(cin >> cmd) || cmd == CMD_QUIT || CMD_NO) {
+		cout << GOODBYE_MSG << endl;
+                return 0;
+        }
+
+	else if (cmd == CMD_YES || cmd == CMD_RESTART) {
+		// clear game data
+		// NEED TO COMPLETE...........................................
+		goto titleScreen;	
+	}
+
+        else {
+                cout << INVALID_CMD_MSG << endl;
+        }
 
 }
 
--- Main/MainHelpers.cc
+++ Main/MainHelpers.cc
@@ -1,30 +1,136 @@
 #include <vector>
+#include <memory>
 #include "Floor.h"
+#include "Direction.h"
+#include "Defines.cc"
 using namespace std;
 
 extern Generator rng;
 extern Floor currentFloor;
+extern vector<shared_ptr<Character>> alreadyActed;
 
 
+
+// checks if coordinates given is a FloorTile or not
 bool isValidCoordinates(vector<int> coord) {
 	return (currentFloor.getCell(coord))->getCellType() 
-		== CellType::FloorTile ? true : false
+		== CellType::FloorTile ? true : false;
 }
 
+
+// checks if coordinates given has an Item or Character on it
 bool isOccupied(vector<int> coord) {
 	return (currentFloor.getCell(coord))->getOccupant()
 		|| (currentFloor.getCell(coord)->getItem()) 
 		? true : false;
 }
 
+
+// wrapper for generator that only returns a valid coordinate
 vector<int> getValidCoordinates() {
+	// safety check no necessary assuming there is enough room for spawning
+
         do {
                 vector<int> tempCoordinates = rng.genCoordinate();
 
                 // reroll if coordinates are not for a FloorTile
                 // or has an occupant already
-        } while (!(isValidCoordinates(tempCoords))
-		|| isOccupied(tempCoords));
+        } while (!(isValidCoordinates(tempCoordinates))
+		|| isOccupied(tempCoordinates));
 
 	return tempCoordinates;
+}
+
+
+// wrapper for generator that only returns a valid neighbouring coordinate
+// given a base coordinate
+// throws if all neighbours are not valid
+vector<int> getValidNeighbourCoordinates(vector<int> base) {
+	// safety check
+	vector<vector<int>> checkedCoordinates
+
+	do {
+                vector<int> tempCoordinates = rng.genNeighbourCoord(base);
+
+		bool checked = false;
+
+                // checks if neighbour was logged
+		for (auto coordinates: checkedCoordinates) {
+                        if (coordinates == tempCoordinates) {
+                                checked = true;
+                                break;
+                        }
+                }
+
+		// if neighbour was not logged, log it
+                if (!(checked)) {
+                        checkedCoordinates.emplace_back(tempCoordinates);
+                }
+
+		// checks if all neighbours were tried
+                if (checkedCoordinates.size() == DIRECTION_ENUMERATION) {
+                        // no valid neighbour
+			throw;
+                }
+
+                // reroll if coordinates are not for a FloorTile
+                // or has an occupant already
+        } while (!(isValidCoordinates(tempCoordinates))
+                || isOccupied(tempCoordinates));
+
+        return tempCoordinates;
+
+}
+
+
+bool hasActed(shared_ptr<Character> character) {
+	for (auto actedCharacter: alreadyActed) {
+		if (actedCharacter == character) {
+			return true;
+		}
+
+	}
+
+	return false;
+}
+
+// wrapper for generator that only returns a valid direction to move to
+// given the current coordinate
+// throws if no valid move exists
+Direction getValidMove(vector<int> base) {
+	// safety check
+	vector<Direction>  checkedDirections;
+
+	do {
+		tempDirection = rng.genDirection();
+		vector<int> tempCoordinates 
+			= (base->getNeighbour(tempDirection))->getCoord();
+
+		bool checked = false;
+
+		// checks if this direction has been logged
+		for (auto direction: checkedDirections) {
+			if (direction == tempDirection) {
+				checked = true;
+				break;
+			}
+		}
+
+		// if directions was not logged, log it
+		if (!(checked)) {
+			checkedDirections.emplace_back(tempDirection);
+		}
+
+		// checks if all directions were tried
+		if (checkedDirections.size() == DIRECTION_ENUMERATION) {
+			// no valid move
+			throw;
+		}
+
+		// reroll if cell in direction is not a FloorTile
+		// or has an occupant already
+	} while (!(isValidCoordinates(tempCoordinates))
+                || isOccupied(tempCoordinates));
+
+	return tempDirection;
 }
