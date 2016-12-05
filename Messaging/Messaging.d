2cfbaf206bbba54209fc1c83da1aa054c2df93c1
yf6wang <yf6wang@student.cs.uwaterloo.ca>
2016-12-05 04:40:00 -0500

    Fixed up reading cmd line args

--- Messaging/Messaging.d
+++
@@ -1,9 +0,0 @@
-Messaging/Messaging.o: Messaging/Messaging.cc \
- Messaging/../Enumerations/Race.h Messaging/../Enumerations/Direction.h \
- Messaging/../Enumerations/ItemType.h \
- Messaging/../Enumerations/PotionType.h Messaging/../Items/Item.h \
- Messaging/../Items/../Enumerations/ItemType.h \
- Messaging/../Items/Potion.h Messaging/../Items/Item.h \
- Messaging/../Items/../Enumerations/PotionType.h \
- Messaging/../Items/GoldPile.h Messaging/../Localisation/Localisation.h \
- Messaging/../Defines/Defines.h
--- ObserverSubject/Subject.d
+++
@@ -1,4 +0,0 @@
-ObserverSubject/Subject.o: ObserverSubject/Subject.cc \
- ObserverSubject/Subject.h ObserverSubject/Observer.h \
- ObserverSubject/../Info/Info.h \
- ObserverSubject/../Info/../Enumerations/Race.h
