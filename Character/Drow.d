1543688090225c9cc724a0131528556696c43973
yf6wang <yf6wang@student.cs.uwaterloo.ca>
2016-12-05 04:40:41 -0500

    Fixed up reading cmd line args

--- Character/Drow.d
+++
@@ -1,6 +0,0 @@
-Character/Drow.o: Character/Drow.cc Character/Drow.h \
- Character/Character.h Character/../ObserverSubject/Subject.h \
- Character/../ObserverSubject/Observer.h \
- Character/../ObserverSubject/../Info/Info.h \
- Character/../ObserverSubject/../Info/../Enumerations/Race.h \
- Character/../Enumerations/Race.h Character/../Defines/Defines.h
