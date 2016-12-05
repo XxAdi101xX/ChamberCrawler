203906e39e0cdba5d5788b68857ed255b8d422d1
yf6wang <yf6wang@student.cs.uwaterloo.ca>
2016-12-04 21:38:20 -0500

    Bug fixes for random movement

--- Items/GoldPile.d
+++
@@ -1,2 +0,0 @@
-Items/GoldPile.o: Items/GoldPile.cc Items/GoldPile.h Items/Item.h \
- Items/../Enumerations/ItemType.h Items/../Defines/Defines.h
--- Items/Item.d
+++
@@ -1,2 +0,0 @@
-Items/Item.o: Items/Item.cc Items/Item.h Items/../Enumerations/ItemType.h \
- Items/../Defines/Defines.h
