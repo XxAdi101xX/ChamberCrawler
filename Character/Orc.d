ff56d329c5d60da72d518b52fa9592e92f81d808
Michael Li <li.michael@live.com>
2016-12-05 05:42:53 -0500

    Removed .d files

--- Character/Orc.d
+++
@@ -1,6 +0,0 @@
-Character/Orc.o: Character/Orc.cc Character/Orc.h Character/Character.h \
-  Character/../ObserverSubject/Subject.h \
-  Character/../ObserverSubject/Observer.h \
-  Character/../ObserverSubject/../Info/Info.h \
-  Character/../ObserverSubject/../Info/../Enumerations/Race.h \
-  Character/../Defines/Defines.h
